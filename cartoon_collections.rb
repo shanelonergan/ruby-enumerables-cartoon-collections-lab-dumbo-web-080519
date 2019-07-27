0324c9093b7911c3af4a7561199753ed3bb98841
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 14:42:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer_calls)
   return caps_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |word| word.length >= 4 }
 end
 
 def find_the_cheese# code an argument here
