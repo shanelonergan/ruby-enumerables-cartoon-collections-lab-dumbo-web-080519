10e2b7d0a3467f7f35a029281150e6d494b688e4
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 14:53:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(calls)
   calls.any? { |word| word.length >= 5 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
