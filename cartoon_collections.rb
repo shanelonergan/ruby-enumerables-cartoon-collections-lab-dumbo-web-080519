5cc718ec5e747c0c5642328e7811713cd3c90091
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 14:32:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ end
 def summon_captain_planet(planeteer_calls)
   caps_array = []
   caps_array = planeteer_calls.map { |calls| calls.capitalize }
-  return caps_array.each_char { |calls| calls + "!" }
+  caps_array = caps_array.each { |calls| calls + "!" }
+  return caps_array
 end
 
 def long_planeteer_calls# code an argument here
