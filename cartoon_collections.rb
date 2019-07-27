beaf3011eac27b99c0fabc4b7af0dfcdbfe5cbb2
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 15:14:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.select { |cheese| cheese_types.include?(array) }
+  array.select { |cheese| array.include?(cheese_types) }
 
 end
