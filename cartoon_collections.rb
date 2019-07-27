6dcf53331cc254ea1aed2492c0b9795f7b71355e
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 15:04:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
-  array.find { |cheese| cheese == cheese_types }
+  overlap = array & cheese_types
+  return overlap
 end
